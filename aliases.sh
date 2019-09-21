e7da540a482c71ae02623bd39bc64527c847bead
Eyal Royee <eyalroyee@gmail.com>
2019-09-21 23:41:41 +0300

    Created a minimal PS1 configuration that could be useful to use within in (by using `:term` in vim).
    Signed-off-by: Eyal Royee <eyalroyee@gmail.com>

--- aliases.sh
+++ aliases.sh
@@ -15,3 +15,6 @@ alias l='ls -CF'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# use the minimal ps1 (useful in terminal within vim)
+alias minimal_ps1="source ${HOME}/.bash/minimal.sh"
---
+++ minimal.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# this file should be sourced *only* after colors.sh was sourced
+# since it uses functions from colors.sh (to show conda env, python virtualenv & git branch name)
+
+PROMPT_COMMAND=__minimal_ps1
+function __minimal_ps1()
+{
+	local EXIT_CODE="$?" # must be first so it will save the *last* executed command exit code.
+	PS1=""
+
+	# First show the conda environment, if any
+	if [[ "${CONDA_PREFIX}" != "" ]]; then
+		conda_env_name_with_color # function from colors.sh
+		PS1+="${CONDA_ENV_NAME_WITH_COLOR}"
+	fi
+
+	# At second show python virtualenv name, if any
+	if [[ "${VIRTUAL_ENV}" != "" ]]; then
+		python_virtualenv_name_with_color # function from colors.sh
+		PS1+="${PYTHON_VIRTUAL_ENV_COLOR_NAME}"
+	fi
+
+	git_branch_with_color
+	PS1+="${GIT_BRANCH_NAME_WITH_COLOR}"
+
+	# add a space before the rest of the PS1
+	if [[ "${PS1}" != "" ]] ; then
+		PS1+=" "
+	fi
+
+	PS1+="${YELLOW}\$ ${RESET_COLOR}" # UID 0 is '#', otherwise '$'
+}
